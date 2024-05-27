638843b855642a86d97ff84ebc4d107fab1313f3
keetrap <parteekkamboj112@gmail.com>
2024-05-27 17:16:04 +0530

    training pipeline updated

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -23,4 +23,4 @@ model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
 
 model_eval_obj = ModelEvaluation()
-model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+model_eval_obj.initiate_model_evaluation(test_arr)
